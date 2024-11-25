52a27d3c1afa839be5a88d4491fcccbfe8095a2e
Olav <73853649+olavorw@users.noreply.github.com>
2024-11-25 11:07:38 -0800

    Restructure project.
    Deleted outdated Header component and Home screen files. This cleanup will streamline the project structure and make it easier to maintain.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import "../global.css";
 import React from "react";
 import ReactDOMClient from "react-dom/client";
-import { Home } from "./screens/Home/Home.jsx";
+import App from "./App.jsx";
 
 const app = document.getElementById("root");
 const root = ReactDOMClient.createRoot(app);
-root.render(<Home />);
+root.render(<App />);
